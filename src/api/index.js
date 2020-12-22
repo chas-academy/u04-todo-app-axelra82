90fe43e3f4b9bae744108fee654fa250aeba1496
Axel Roussille Åberg <axelra82@gmail.com>
2020-12-22 23:19:10 +0100

    Update frontend api for better flex

--- src/api/index.js
+++ src/api/index.js
@@ -4,25 +4,37 @@ import axios from 'axios';
 
 export default async (
 	endpoint = null,
-	data = null,
+	data = {},
 ) => {
 
 	// Init let vars
 	let endpointLocation,
-		response;
+		response,
+		endpointUrl;
 
 	// Endpoint case switch
 	switch (endpoint) {
 		case 'configure':
 			endpointLocation = 'Configure/Init.php';
+			// Endpoint has unknown variables at this point
+			// Use data object to build endpoint during configure
+			endpointUrl = `http://${data.host}:${data.port}/backend/API/Endpoint/${endpointLocation}`
 			break;
+		case 'task-read':
+			endpointLocation = 'Task/Read.php';
+			break;
+	}
+
+	// Configured endpoint URL
+	if (endpoint !== 'configure') {
+		endpointUrl = `http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/backend/API/Endpoint/${endpointLocation}`
 	}
 
 	try {
 
 		// Set up API request
 		const options = {
-			url: `http://${data.host}:${data.port}/backend/API/Endpoint/${endpointLocation}`,
+			url: endpointUrl,
 			method: 'POST',
 			headers: {
 				'Access-Control-Allow-Origin': '*',
@@ -34,6 +46,10 @@ export default async (
 		// Await api response from request
 		const request = await axios(options);
 		// Success object
+		request.data.data = typeof request.data.data !== "undefined" ?
+			JSON.parse(request.data.data)
+			:
+			{};
 		response = request.data
 
 	} catch (error) {
