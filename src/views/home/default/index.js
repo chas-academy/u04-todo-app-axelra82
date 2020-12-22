482bd694983259fc25b54530b790e353314bcc7f
Axel Roussille Åberg <axelra82@gmail.com>
2020-12-22 23:21:13 +0100

    Start frontend backend communicaiton with simple single task

--- src/views/home/default/index.js
+++ src/views/home/default/index.js
@@ -1,9 +1,27 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../../api';
+import { TaskList } from '../../../components/tasks';
 
 export default () => {
+	const [tasks, setTasks] = useState(false);
+
+	// Used to refresh task lists
+	const getTasks = async () => {
+		const response = await api('task-read');
+		console.log(response);
+		setTasks(response.data);
+		return;
+	}
+
+	// Run once initially using empty array
+	useEffect(() => {
+		getTasks();
+	}, []);
 
 	return (
-		<div>Placeholder</div>
+		<div className="container">
+			<TaskList props={{ tasks, getTasks }} />
+		</div>
 	);
 }
